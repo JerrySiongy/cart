232ee306393e6183d9a6b3b32e1df9381378b3d4
jerry <jksiongy@gmail.com>
2026-08-27 18:31:31 +0300

    my third commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+app.secret_key = 'asjnjkabsfkasdnfafhaiouefaoufheuafejfa230029jrysabjfjasdafaosiongy254'
+
+@app.route('/')
+def home():
+
+    return render_template('index.html')
+
+
+app.run(debug=True)
